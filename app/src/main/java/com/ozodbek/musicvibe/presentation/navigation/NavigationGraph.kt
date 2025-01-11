5b7409c92757f79478cd39aa0c552f780d1d5a77
ozodbek-karimov <ozodbekkarimov100@gmail.com>
2025-01-11 12:28:10 +0100

    AudioScreen implemented

--- app/src/main/java/com/ozodbek/musicvibe/presentation/navigation/NavigationGraph.kt
+++ app/src/main/java/com/ozodbek/musicvibe/presentation/navigation/NavigationGraph.kt
@@ -1,13 +1,19 @@
 package com.ozodbek.musicvibe.presentation.navigation
 
+import android.net.Uri
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.ozodbek.musicvibe.presentation.screens.AudioFilesScreen
+import com.ozodbek.musicvibe.presentation.screens.AudioFilesViewModel
 import com.ozodbek.musicvibe.presentation.screens.PlaySongScreen
+import com.ozodbek.musicvibe.presentation.screens.SharedSongPlayerViewModel
+import org.koin.androidx.compose.koinViewModel
 
 
 /**
@@ -24,16 +30,31 @@ fun NavigationGraph(
     val navHostController = rememberNavController()
 
 
+    val sharedViewModel = koinViewModel<SharedSongPlayerViewModel>()
+
+    val currentSelectedSong by sharedViewModel.currentSelectedSong.collectAsStateWithLifecycle()
     NavHost(
         navController = navHostController,
         startDestination = Screens.AudioFilesScreen,
         modifier = modifier
     ) {
         composable<Screens.AudioFilesScreen> {
+            val viewModel = koinViewModel<AudioFilesViewModel>()
+
+            val files by viewModel.audioFiles.collectAsStateWithLifecycle()
+            val sortState by viewModel.sortState.collectAsStateWithLifecycle()
+
             AudioFilesScreen(
-                onNavigateToPlaySong = { songUrl ->
-                    navHostController.navigate(Screens.PlaySongScreen(songUrl))
-                }
+                onNavigateToPlaySong = { songUri ->
+                    val encodedUri = Uri.encode(songUri)
+                    navHostController.navigate(Screens.PlaySongScreen(encodedUri))
+                },
+                currentSong = currentSelectedSong,
+                music = files,
+                sortState = sortState,
+                onSortEvents = viewModel::onSortEvents,
+                onItemSelect = sharedViewModel::onSongSelect,
+                onSongEvents = sharedViewModel::onPlaySongEvents,
             )
         }
         composable<Screens.PlaySongScreen> { backStackEntry ->
---
+++ app/src/main/java/com/ozodbek/musicvibe/presentation/screens/AudioFilesViewModel.kt
@@ -0,0 +1,69 @@
+package com.ozodbek.musicvibe.presentation.screens
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ozodbek.musicvibe.domain.models.MusicResourceModel
+import com.ozodbek.musicvibe.domain.music.MusicFileReader
+import com.ozodbek.musicvibe.presentation.util.MusicSortOrder
+import com.ozodbek.musicvibe.presentation.util.states.ChangeSortOrderEvents
+import com.ozodbek.musicvibe.presentation.util.states.MusicSortState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class AudioFilesViewModel(
+    private val reader: MusicFileReader,
+) : ViewModel() {
+
+    private val _files = MutableStateFlow<List<MusicResourceModel>>(emptyList())
+    val audioFiles = _files.asStateFlow()
+
+    private val _sortState = MutableStateFlow(MusicSortState())
+    val sortState = _sortState.asStateFlow()
+
+    init {
+        loadFiles()
+    }
+
+
+    fun onSortEvents(events: ChangeSortOrderEvents) {
+        when (events) {
+            is ChangeSortOrderEvents.OnOrderChanged -> onSortOrderChanged(events.order)
+            ChangeSortOrderEvents.ToggleDialogState -> onToggleSortDialog()
+        }
+    }
+
+    private fun onToggleSortDialog() =
+        _sortState.update { it.copy(isDialogOpen = !it.isDialogOpen) }
+
+    private fun onSortOrderChanged(order: MusicSortOrder) {
+        _sortState.update { it.copy(sortOrder = order) }
+        when (order) {
+            MusicSortOrder.AscendingByDuration -> _files.update { files ->
+
+                files.sortedBy { it.duration }
+            }
+
+            MusicSortOrder.CreatedAtAscending -> _files.update { files ->
+                files.sortedBy { it.createdAt }
+            }
+
+            MusicSortOrder.CreatedAtDescending -> _files.update { files ->
+                files.sortedByDescending { it.createdAt }
+            }
+
+            MusicSortOrder.DescendingByDuration -> _files.update { files ->
+                files.sortedByDescending { it.duration }
+            }
+        }
+    }
+
+    private fun loadFiles() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val files = reader.readMusicFiles()
+            _files.update { files }
+        }
+    }
+}
